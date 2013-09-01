1ec4c272f5e3b2067f5ff6f0234dfad018427995
Matthew McFarland <mmcfarland@gmail.com>
2013-08-31 21:57:11 -0400

    Handle edge effects

--- elemental-ca.js
+++ elemental-ca.js
@@ -31,18 +31,21 @@ function draw(h) {
 }
 
 function generate() {
-  var newgen = [];
+  var newgen = [],
+      end = cells.length -1;
+  
   for(var i=0; i < cells.length; i++) {
-    l = cells[i-1];
+    l = i === 0 ? 0 : cells[i-1];
     c = cells[i];
-    r = cells[i+1];
+    r = i === end ? end : cells[i+1];
     newgen[i] = rules(l, c, r);
   }
   cells = newgen;
 }
 
-for (var i=0; i < gens; i++) {
-  draw(i);
+for (var g=0; g < gens; g++) {
+  console.log('gen: ' + g + ' of ' + gens);
+  draw(g);
   generate();  
 }
 
